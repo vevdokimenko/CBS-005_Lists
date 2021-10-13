0d440c54890af4af4c12050eafeb472a659d7a1d
vevdokimenko <evdokimenko@hotmail.com>
2021-10-13 14:55:19 +0300

    Homework. Task2

---
+++ 005_Samples/src/homework/task2/Zoo.java
@@ -0,0 +1,19 @@
+package homework.task2;
+
+import java.util.ArrayList;
+
+public class Zoo {
+    public static void main(String[] args) {
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add(0,"Bull");
+        animals.add(1,"Bear");
+        animals.add(2,"Crocodile");
+        animals.add(3,"Wolf");
+        animals.add(4,"Lion");
+        animals.add(5,"Elephant");
+        animals.add(6,"Horse");
+        animals.add(7,"Leopard");
+
+        System.out.println(animals);
+    }
+}
